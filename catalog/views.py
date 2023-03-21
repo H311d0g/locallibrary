b811331c1b3c804b762e13bdc86a94e43ea3c781
Gabriel Oshee <gnavarreteoshee@gmail.com>
2023-03-21 20:36:26 -0300

    Adding simply index

--- catalog/views.py
+++ catalog/views.py
@@ -6,16 +6,16 @@ def index(request):
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-
     num_authors = Author.objects.count()
+    num_genres = Genre.objects.count()
 
     context = {
         'num_books':num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres': num_genres,
     }
 
     return render(request, 'index.html', context=context)
