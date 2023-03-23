c2567b88aa3139101ea68f10fe460c6c32ffc603
Gabriel Oshee <gnavarreteoshee@gmail.com>
2023-03-23 20:52:17 -0300

    Adding templates/catalog

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 # Create your views here.
 def index(request):
     """View function for home page site"""
@@ -19,3 +20,10 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'book_list' #own name for the list as a template variable
+    queryset = Book.objects.filter(title__icontains='war')[:5] #Get 5 books containing the title war
+    template_name = 'books/my_arbitrary_template_name_list.html' #Specify Ur own template name/location
+    
